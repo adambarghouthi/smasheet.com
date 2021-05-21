4c162e1796bde08e6fdd97957b0514ed7cd689b3
Adam Albarghouthi <adambargh@gmail.com>
2021-05-20 20:08:59 -0400

    mock get spreadsheets

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import jwt from "next-auth/jwt";
+const drive = require("@googleapis/drive");
 
-export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const secret = process.env.SECRET;
+
+export default async function helloAPI(req, res) {
+  const token = await jwt.getToken({ req, secret });
+
+  try {
+    const authClient = await drive.auth.getClient({
+      scopes: ["https://www.googleapis.com/auth/drive"],
+    });
+    authClient.setCredentials({
+      access_token: token.gapiAccessToken,
+      refresh_token: token.gapiRefreshToken,
+    });
+
+    const client = await drive.drive({
+      version: "v3",
+      auth: authClient,
+    });
+
+    const createResponse = await client.files.list({
+      q: "mimeType='application/vnd.google-apps.spreadsheet'",
+    });
+
+    res.status(200).json(createResponse.data.files);
+
+    console.log(createResponse.data);
+  } catch (error) {
+    console.log(error);
+    res.status(403).json({ message: error.message });
+  }
 }
